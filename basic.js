42fb4faf103cb28a87cd7d4214088c1fee3ee352
Daniel Dallas Okoye <39765403+DanielDallas@users.noreply.github.com>
2024-04-06 10:59:04 +0000

    Further edits

--- basic.js
+++ basic.js
@@ -14,15 +14,36 @@
 // const averaggeAge = (ageDaniel + ageMicheal) / 2;
 // console.log(averaggeAge, ageDaniel, ageMicheal);
 
-const weightEmeka = 78;
-const heightEmeka = 1.69;
-const weightJenny = 92;
-const heightJenny = 1.95;
+// const weightEmeka = 78;
+// const heightEmeka = 1.69;
+// const weightJenny = 92;
+// const heightJenny = 1.95;
 
-console.log(weightEmeka, weightJenny, heightEmeka, heightJenny);
+// console.log(weightEmeka, weightJenny, heightEmeka, heightJenny);
 
-BMIEmeka = (weightEmeka / (heightEmeka * heightEmeka)).toFixed(2);
-BMIJenny = (weightJenny / (heightJenny * heightJenny)).toFixed(2);
+// BMIEmeka = (weightEmeka / (heightEmeka * heightEmeka)).toFixed(2);
+// BMIJenny = (weightJenny / (heightJenny * heightJenny)).toFixed(2);
 
-console.log("Emeka's BMI is:" + " " + BMIEmeka);
-console.log("Jenny's BMI is:" + " " + BMIJenny);
+// console.log("Emeka's BMI is:" + " " + BMIEmeka);
+// console.log("Jenny's BMI is:" + " " + BMIJenny);
+
+const firstName = "Davis";
+const lastName = "Ogbonna";
+const job = "Dev";
+const birthYear = 1993;
+const now = 2024;
+
+const whoIs = `I'm ${firstName} ${lastName}, a ${now - birthYear} old ${job}.`;
+
+console.log(whoIs);
+
+const pinNumberSuffix = `DSC`;
+let pinNumber = 132980;
+if (pinNumber < 132988) {
+  console.log(
+    `The PIN number for Davis Ogbonna is:${pinNumberSuffix + pinNumber}`
+  );
+  pinNumber++;
+} else {
+  console.log(`No more Pin numbers available`);
+}
