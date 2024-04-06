bc582dfab7d2be6138704e8eb01fe8b149f25700
Daniel Dallas Okoye <39765403+DanielDallas@users.noreply.github.com>
2024-04-06 01:21:32 +0000

    Initial boilerplate

---
+++ basic.js
@@ -0,0 +1,28 @@
+// alert("Testing");
+
+// let x = 5;
+// alert(x);
+
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+// const now = 2024;
+// const ageDaniel = 1968;
+// const ageMicheal = 1987;
+
+// const averaggeAge = (ageDaniel + ageMicheal) / 2;
+// console.log(averaggeAge, ageDaniel, ageMicheal);
+
+const weightEmeka = 78;
+const heightEmeka = 1.69;
+const weightJenny = 92;
+const heightJenny = 1.95;
+
+console.log(weightEmeka, weightJenny, heightEmeka, heightJenny);
+
+BMIEmeka = (weightEmeka / (heightEmeka * heightEmeka)).toFixed(2);
+BMIJenny = (weightJenny / (heightJenny * heightJenny)).toFixed(2);
+
+console.log("Emeka's BMI is:" + " " + BMIEmeka);
+console.log("Jenny's BMI is:" + " " + BMIJenny);
